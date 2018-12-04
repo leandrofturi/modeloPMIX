c3435783ce7f0aa3fb5bfdffcf76dd2300bfa5ee
Leandro Furlam <44095440+leandrofturi@users.noreply.github.com>
2018-12-04 17:31:00 -0200

    Faltam ajustes na geração da serie

--- PMIX_construcao.R
+++ PMIX_construcao.R
@@ -63,12 +63,14 @@ correlograma = function(serie, lagMax, grafico){
 }
 
 residuos = function(serie, parametros, lags){
-  nyrs = length(serie)/12
+  nTotal = length(serie)
+  serieV = numeric(nTotal)
+  serieV = as.vector(t(serie))
   p = lags[1]
   P = lags[2]
   q = lags[3]
   Q = lags[4]
-  lagMax = max(p, P, q, Q)
+  lagMax = 12*(max(P, Q) + 1)
   
   limInf = 1
   limSup = 12*p
@@ -83,7 +85,7 @@ residuos = function(serie, parametros, lags){
   limSup = limInf + 12*Q - 1
   THT = matrix(parametros[limInf : limSup], ncol = 12, byrow = T)
   
-  residuo = matrix(0, ncol = 12, nrow = nyrs)
+  residuoV = numeric(nTotal)
   dpRes = numeric(12)
   autoMensal = numeric(1)
   autoAnual = numeric(1)
@@ -91,61 +93,60 @@ residuos = function(serie, parametros, lags){
   mmMensal = numeric(1)
   mmAnual = numeric(1)
   mmMensalAnual = numeric(1)
-
   
-  for(v in lagMax+1 : (nyrs - 1)){
-    for(t in 1 : 12){
-      autoMensal = 0
-      autoAnual = 0
-      autoMensalAnual = 0
-      mmMensal = 0
-      mmAnual = 0
-      mmMensalAnual = 0
-      
-      if (p > 0) {
-        for(i in 1 : p){
-          tlag = (t - i - 1) %% 12 + 1
-          autoMensal = autoMensal + phi[i, t] * serie[v, tlag]
-            
-          if (P > 0) {
-            for(k in 1 : P){
-                vlag = v - k
-                autoMensalAnual = autoMensalAnual + phi[i, t] * PHI[k, t] * serie[vlag, tlag]
-            }
+  for(t in (lagMax : nTotal)) {
+    mes = t %% 12
+    if(mes == 0) mes = 12
+    
+    autoMensal = 0
+    autoAnual = 0
+    autoMensalAnual = 0
+    mmMensal = 0
+    mmAnual = 0
+    mmMensalAnual = 0
+    
+    if (p > 0) {
+      for(i in 1 : p){
+        tlag = t - i
+        autoMensal = autoMensal + phi[i, mes]*serieV[tlag]
+        
+        if (P > 0) {
+          for(k in 1 : P){
+            vtlag = (t - k*12) - i
+            autoMensalAnual = autoMensalAnual + phi[i, mes]*PHI[k, mes]*serieV[vtlag]
           }
         }
       }
-      if (P > 0) {
-        for(j in 1 : P){
-          vlag = v - j
-          autoAnual = autoAnual + PHI[j, t] * serie[vlag, t]
-        }
-      }
-      residuo[v, t] = serie[v, t] - autoMensal - autoAnual + autoMensalAnual
-      
-      if (q > 0) {
-        for(l in 1 : q){
-          tlag = (t - l - 1) %% 12 + 1
-          mmMensal = mmMensal + tht[l, t] * residuo[v, tlag]
-          
-          if (Q > 0) {
-            for(n in 1 : Q){
-              vlag = v - n
-              mmMensalAnual = mmMensalAnual + tht[l, t] * THT[n, t] * residuo[vlag, tlag]
-            }
-          }
-        }
-      }
-      
-      if (Q > 0) {
-        for(m in 1 : Q){
-          vlag = v - m
-          mmAnual = mmAnual + THT[m, t] * residuo[vlag, t]
-        }
-      }
-      residuo[v, t] = residuo[v, t] + mmMensal + mmAnual - mmMensalAnual
     }
+    if (P > 0) {
+      for(j in 1 : P){
+        vlag = t - j*12
+        autoAnual = autoAnual + PHI[j, mes]*serieV[vlag]
+      }
+    }
+    if (q > 0) {
+      for(l in 1 : q){
+        tlag = t - l
+        mmMensal = mmMensal + tht[l, mes]*residuoV[tlag]
+        if (Q > 0) {
+          for(n in 1 : Q){
+            vtlag = (t - n*12) - l
+            mmMensalAnual = mmMensalAnual + tht[l, mes]*THT[n, mes]*residuoV[vtlag]
+          }
+        }
+      }
+    }
+    
+    if (Q > 0) {
+      for(m in 1 : Q){
+        vlag = t - m*12
+        mmAnual = mmAnual + THT[m, mes]*residuoV[vlag]
+      }
+    }
+    residuoV[t] = serieV[t] - autoMensal - autoAnual + autoMensalAnual + mmMensal + mmAnual - mmMensalAnual
   }
+  residuo = matrix(residuoV, ncol = 12, byrow = T)
+  
   dpRes = apply(residuo, 2, sd)
   somQuadRes = sum(residuo ^ 2)
   
@@ -153,81 +154,69 @@ residuos = function(serie, parametros, lags){
   return(final)
 }
 
-
-otimizacao = function(serie, lags){
-  nyrs = length(serie)/12
+powell = function (serie, lags) {
+  
+  eps = 10^-5
   p = lags[1]
   P = lags[2]
   q = lags[3]
   Q = lags[4]
   ordem = sum(lags)
   
+  P0 = c(rep(0, (p*12)), rep(0, (q*12)), rep(0, (P*12)), rep(0, (Q*12)))
   passo = matrix(numeric(1), ncol = (ordem*12), nrow = (ordem*12))
-  P0 = numeric(ordem*12)
-  E0 = numeric(1)
-  Pi = numeric(ordem*12)
-  Eiant = numeric(1)
-  Ei = numeric(1)
-  Pdec = numeric(1)
-  delta = numeric(1)
-  Pe = numeric(ordem * 12)
-  Ee = numeric(1)
-  sp = numeric(ordem*12)
-  
-  difRes = 10
-  ciclos = 0
   for (i in 1 : (ordem*12)) passo[i, i] = 1
-  P0 = rep(0, (ordem * 12))
-  P0 = c(rep(1, (p*12)), rep(0, (q*12)), rep(1, (P*12)), rep(0, (Q*12)))
-  
   E0 = residuos(serie, P0, lags)$somRes
+  Pi = P0
+  Ei = E0
   
-  while ((difRes > 10 ^-6) && (ciclos <= 100)) {
-      delta = 0
-    
-    for (i in 1 : (ordem*12)) {
-      if (i == 1) {
-        Pi = P0
-        Eiant = E0
-      }
-      else Eiant = Ei
+  for (ciclos in (1 : 100)) {
+    Pdec = 1
+    delta = 0
+    print(E0)
+    for (i in (1 : (ordem*12))) {
+      Eiant = Ei
       busca = buscaLinear(serie, lags, Pi, passo[, i])
       Pi = busca$ponto
       Ei = busca$E
       
-      if ((abs(Ei - Eiant)) > delta) {
-        delta = abs(Ei - Eiant)
+      if (Eiant - Ei > delta) {
+        delta = Eiant - Ei
         Pdec = i
       }
     }
-    sp = Pi - P0
+    if (abs(Ei - E0) < eps) {
+      final = list(parametros = Pi, SomRes = Ei)
+      return (final)
+    }
     Pe = 2*Pi - P0
     Ee = residuos(serie, Pe, lags)$somRes
-    
     if (Ee < E0) {
-      if  ((2*(E0 - 2*Ei + Ee)*((E0 - Ei - delta)^2)) < (((E0 - Ee)^2)*delta)) {
-        if (Pdec < ordem*12)  passo[, (Pdec:((ordem*12)-1))] = passo[, ((Pdec+1):(ordem*12))]
-        passo[, (ordem*12)] = sp
-        busca = buscaLinear(serie, lags, Pi, sp)
-        Pi = busca$ponto
-        Ei = busca$E
+      if (2*(E0 - 2*Ei + Ee)*((E0 - Ei - delta)^2) < ((E0 - Ee)^2)*delta) {
+        S = Pi - P0
+        busca = buscaLinear(serie, lags, S, Pi)
+        Ps = busca$ponto
+        Es = busca$E
+        if (Pdec < ordem*12) passo[, (Pdec:((ordem*12)-1))] = passo[, ((Pdec+1):(ordem*12))]
+        passo[, (ordem*12)] = S
+        Pi = Ps
+        Ei = Es
       }
     }
-    difRes = abs(Ei - E0)
-    E0 = Ei
+    if (Ei > E0) {
+      final = list(parametros = Pi, SomRes = Ei)
+      return (final)
+    }
     P0 = Pi
-    print(E0) #######################################################################
-    
-    ciclos = ciclos + 1
+    E0 = Ei
   }
-  dpRes = residuos(serie, P0, lags)$dpRes
-  final = list(ponto = P0, dpRes = dpRes)
+  final = list(parametros = Pi, SomRes = Ei)
   return (final)
 }
 
 
 secaoAurea = function (serie, lags, ponto, passo) {
-  #OURO = (sqrt(5) - 1) / 2
+  OURO = (sqrt(5) - 1) / 2
   OURO = 5
   OUROlim = 100
   
@@ -250,64 +239,69 @@ secaoAurea = function (serie, lags, ponto, passo) {
   c = b + (1 + OURO)*(b - a)
   novoPonto = ponto + (c * passo)
   Ec = residuos(serie, novoPonto, lags)$somRes
+  
   while (Eb > Ec) {
-    r = (b - a)*(Eb - Ec)
-    q = (b - c)*(Eb - Ea)
-    if ((q - r) >= 0) div = 2 * (max(abs(q - r), 10^-20))
-    else div = (-2) * (max(abs(q - r), 10^-20))
-    u = b - ((b - c)*q - (b - a)*r) / div
-    limu = b + OUROlim*(c - b)
-    
-    if ((b - u)*(u - c) > 0) {
-      novoPonto = ponto + (u * passo)
-      Eu = residuos(serie, novoPonto, lags)$somRes
-      if (Eu < Ec) {
+    finalAnt = list(a = a, Ea = Ea, b = b, Eb = Eb, c = c, Ec = Ec)
+    for (ciclos in 1:100) {
+      r = (b - a)*(Eb - Ec)
+      q = (b - c)*(Eb - Ea)
+      if ((q - r) >= 0) div = 2 * (max(abs(q - r), 10^-20))
+      else div = (-2) * (max(abs(q - r), 10^-20))
+      u = b - ((b - c)*q - (b - a)*r) / div
+      limu = b + OUROlim*(c - b)
+      
+      if ((b - u)*(u - c) > 0) {
+        novoPonto = ponto + (u * passo)
+        Eu = residuos(serie, novoPonto, lags)$somRes
+        if (Eu < Ec) {
+          a = b
+          b = u
+          Ea = Eb
+          Eb = Eu
+          final = list(a = a, Ea = Ea, b = b, Eb = Eb, c = c, Ec = Ec)
+          return (final)
+        }
+        else if (Eu > Eb) {
+          c = u
+          Ec = Eu
+          final = list(a = a, Ea = Ea, b = b, Eb = Eb, c = c, Ec = Ec)
+          return (final)
+        }
+        u = c + (1 + OURO)*(c - b)
+        novoPonto = ponto + (u * passo)
+        Eu = residuos(serie, novoPonto, lags)$somRes
+      }
+      else if ((c - u)*(u - limu) > 0) {
+        novoPonto = ponto + (u * passo)
+        Eu = residuos(serie, novoPonto, lags)$somRes
+        if (Eu < Ec) {
+          b = c
+          c = u
+          u = c + (1 + OURO)*(c - b)
+          Eb =  Ec
+          Ec = Eu
+          novoPonto = ponto + (u * passo)
+          Eu = residuos(serie, novoPonto, lags)$somRes
+        }
+        else if ((u - limu)*(limu - c) >= 0) {
+          u = limu
+          novoPonto = ponto + (u * passo)
+          Eu = residuos(serie, novoPonto, lags)$somRes
+        }
+        else {
+          u = c + (1 + OURO)*(c - b)
+          novoPonto = ponto + (u * passo)
+          Eu = residuos(serie, novoPonto, lags)$somRes 
+        }
         a = b
-        b = u
-        Ea = Eb
-        Eb = Eu
-        final = list(a = a, Ea = Ea, b = b, Eb = Eb, c = c, Ec = Ec)
-        return (final)
-      }
-      else if (Eu > Eb) {
-        c = u
-        Ec = Eu
-        final = list(a = a, Ea = Ea, b = b, Eb = Eb, c = c, Ec = Ec)
-        return (final)
-      }
-      u = c + (1 + OURO)*(c - b)
-      novoPonto = ponto + (u * passo)
-      Eu = residuos(serie, novoPonto, lags)$somRes
-    }
-    else if ((c - u)*(u - limu) > 0) {
-      novoPonto = ponto + (u * passo)
-      Eu = residuos(serie, novoPonto, lags)$somRes
-      if (Eu < Ec) {
         b = c
         c = u
-        u = c + (1 + OURO)*(c - b)
-        Eb =  Ec
+        Ea = Eb
+        Eb = Ec
         Ec = Eu
-        novoPonto = ponto + (u * passo)
-        Eu = residuos(serie, novoPonto, lags)$somRes
       }
-      else if ((u - limu)*(limu - c) >= 0) {
-        u = limu
-        novoPonto = ponto + (u * passo)
-        Eu = residuos(serie, novoPonto, lags)$somRes
-      }
-      else {
-        u = c + (1 + OURO)*(c - b)
-        novoPonto = ponto + (u * passo)
-        Eu = residuos(serie, novoPonto, lags)$somRes 
-      }
-      a = b
-      b = c
-      c = u
-      Ea = Eb
-      Eb = Ec
-      Ec = Eu
     }
+    return (finalAnt)
   }
   final = list(a = a, Ea = Ea, b = b, Eb = Eb, c = c, Ec = Ec)
   return (final)
@@ -340,7 +334,7 @@ metBrent = function (serie, lags, ponto, passo, buscaDourada) {
   
   for (iteracoes in 1:MAXIT) {
     xm = (a + b) / 2
-    tol1 = 2*10^-10 * abs(x) + 10^-10
+    tol1 = 2*10^-6 * abs(x) + 10^-10
     tol2 = 2 * tol1
     if (abs(x - xm) <= (tol2 - 0.5*(b - a))) {
       xmin = x
@@ -414,7 +408,7 @@ metBrent = function (serie, lags, ponto, passo, buscaDourada) {
   final = list(ponto = xmin, Ex = Ex)
   return (final)
 }
-  
+
 buscaLinear = function (serie, lags, ponto, passo) {
   buscaDourada = secaoAurea(serie, lags, ponto, passo)
   metBrent = metBrent(serie, lags, ponto, passo, buscaDourada)
@@ -427,40 +421,48 @@ buscaLinear = function (serie, lags, ponto, passo) {
 }
 
 serieSint = function(serie, lags, n) {
-  
-  otimizados = otimizacao(serie, lags)
-  parametros = otimizados$ponto
-  dpRes = otimizados$dpRes
-  #parametros = c(0.883429, 0.704449, 0.775853, 0.360193, 0.841805, 0.879542, 0.855069, 0.9339, 0.958456, 0.927165, 0.897832, 0.844401,
-  #              0.337275, -0.17647, 0.146482, -0.361297, 0.006054, 0.122416, -0.256529, -0.189037, -0.403412, 0.442112,-0.316306, -0.27799,
-  #               0.697471, -0.547037, -0.681417, 0.162175, -0.588748, 0.678744, 0.876987, 0.710564, 0.164374, -0.975155, 0.41605, 0.953006,
-  #               0.835418, -1.325291, -0.445427, 0.021704, -0.73116, 0.431762, 0.651614, 0.568605, 0.144673, -1.364278, 0.861876, 1.332262)
-  #dpRes = c(0.62, 0.53, 0.68, 0.8, 0.51, 0.36, 0.22, 0.14, 0.22, 0.2, 0.37, 0.36)
-    
+  nTotal = 12*n
   p = lags[1]
   P = lags[2]
   q = lags[3]
   Q = lags[4]
   lagMax = max(p, P, q, Q)
   
-  limInf = 1
-  limSup = 12*p
-  phi = matrix(parametros[limInf : limSup], ncol = 12, byrow = T)
-  print(phi)
-  limInf = limSup + 1
-  limSup = limInf + 12*q - 1
-  tht = matrix(parametros[limInf : limSup], ncol = 12, byrow = T)
-  print(tht)
-  limInf = 12*q + limInf
-  limSup = limInf + 12*P - 1
-  PHI = matrix(parametros[limInf : limSup], ncol = 12, byrow = T)
-  print(PHI)
-  limInf = 12*P + limInf
-  limSup = limInf + 12*Q - 1
-  THT = matrix(parametros[limInf : limSup], ncol = 12, byrow = T)
-  print(THT)
+  otimizacao = powell(serie, lags)
+  parametros = otimizacao$parametros
+  print(parametros)
+  #parametros = c(0.883429, 0.704449, 0.775853, 0.360193, 0.841805, 0.879542, 0.855069, 0.9339, 0.958456, 0.927165, 0.897832, 0.844401,
+  #              0.337275, -0.17647, 0.146482, -0.361297, 0.006054, 0.122416, -0.256529, -0.189037, -0.403412, 0.442112,-0.316306, -0.27799,
+  #               0.697471, -0.547037, -0.681417, 0.162175, -0.588748, 0.678744, 0.876987, 0.710564, 0.164374, -0.975155, 0.41605, 0.953006,
+  #               0.835418, -1.325291, -0.445427, 0.021704, -0.73116, 0.431762, 0.651614, 0.568605, 0.144673, -1.364278, 0.861876, 1.332262)
+  #dpRes = c(0.62, 0.53, 0.68, 0.8, 0.51, 0.36, 0.22, 0.14, 0.22, 0.2, 0.37, 0.36)
+  limInf = 0
+  limSup = 1
+  if (p > 0) {
+    limInf = 1
+    limSup = 12*p
+    phi = matrix(parametros[limInf : limSup], ncol = 12, byrow = T)
+    print(phi)
+  }
+  if (q > 0) {
+    limInf = limSup + 1
+    limSup = limInf + 12*q - 1
+    tht = matrix(parametros[limInf : limSup], ncol = 12, byrow = T)
+  }
+  if (P > 0) {
+    limInf = 12*q + limInf
+    limSup = limInf + 12*P - 1
+    PHI = matrix(parametros[limInf : limSup], ncol = 12, byrow = T)
+    print(PHI)
+  }
+  if (Q > 0) {
+    limInf = 12*P + limInf
+    limSup = limInf + 12*Q - 1
+    THT = matrix(parametros[limInf : limSup], ncol = 12, byrow = T)
+  }
   
-  serieS = matrix(numeric(1), ncol = 12, nrow = n)
+  serieS = matrix(numeric(0), ncol = 12, nrow = n)
+  serieSV = numeric(12*n)
   dpH = numeric(12)
   dpS = numeric(12)
   mediaH = numeric(12)
@@ -471,6 +473,7 @@ serieSint = function(serie, lags, n) {
   sdAleat = apply(residuoS, 2, sd)
   residuoS = t((t(residuoS) - meanAleat) / sdAleat)
   residuoS = t(t(residuoS) * dpRes)
+  residuoSV = as.vector(t(residuoS))
   
   autoMensal = numeric(1)
   autoAnual = numeric(1)
@@ -479,60 +482,61 @@ serieSint = function(serie, lags, n) {
   mmAnual = numeric(1)
   mmMensalAnual = numeric(1)
   
-  for(v in lagMax+1 : (n - 1)){
-    for(t in 1 : 12){
-      autoMensal = 0
-      autoAnual = 0
-      autoMensalAnual = 0
-      mmMensal = 0
-      mmAnual = 0
-      mmMensalAnual = 0
-      
-      if (p > 0) {
-        for(i in 1 : p){
-          tlag = (t - i - 1) %% 12 + 1
-          autoMensal = autoMensal + phi[i, t] * serieS[v, tlag]
-          
-          if (P > 0) {
-            for(k in 1 : P){
-              vlag = v - k
-              autoMensalAnual = autoMensalAnual + phi[i, t] * PHI[k, t] * serieS[vlag, tlag]
-            }
+  for(t in ((lagMax + 1) : nTotal)) {
+    mes = t %% 12
+    if(mes == 0) mes = 12
+    
+    autoMensal = 0
+    autoAnual = 0
+    autoMensalAnual = 0
+    mmMensal = 0
+    mmAnual = 0
+    mmMensalAnual = 0
+    
+    if (p > 0) {
+      for(i in 1 : p){
+        tlag = t - i
+        autoMensal = autoMensal + phi[i, mes]*serieSV[tlag]
+        
+        if (P > 0) {
+          for(k in 1 : P){
+            vtlag = (t - k*12) - i
+            autoMensalAnual = autoMensalAnual + phi[i, mes]*PHI[k, mes]*serieSV[vtlag]
+            
           }
         }
       }
-      
-      if (P > 0) {
-        for(j in 1 : P){
-          vlag = v - j
-          autoAnual = autoAnual + PHI[j, t] * serieS[vlag, t]
-        }
-      }
-      
-      if (q > 0) {
-        for(l in 1 : q){
-          tlag = (t - l - 1) %% 12 + 1
-          mmMensal = mmMensal + tht[l, t] * residuoS[v, tlag]
-          
-          if (Q > 0) {
-            for(n in 1 : Q){
-              vlag = v - n
-              mmMensalAnual = mmMensalAnual + tht[l, t] * THT[n, t] * residuoS[vlag, tlag]
-            }
-          }
-        }
-      }
-      
-      if (Q > 0) {
-        for(m in 1 : Q){
-          vlag = v - m
-          mmAnual = mmAnual + THT[m, t] * residuoS[vlag, t]
-        }
-      }
-      
-      serieS[v, t] = residuoS[v, t] + autoMensal + autoAnual - autoMensalAnual - mmMensal - mmAnual + mmMensalAnual
     }
+    if (P > 0) {
+      for(j in 1 : P){
+        vlag = t - j*12
+        autoAnual = autoAnual + PHI[j, mes]*serieSV[vlag]
+      }
+    }
+    
+    if (q > 0) {
+      for(l in 1 : q){
+        tlag = t - l
+        mmMensal = mmMensal + tht[l, mes] * residuoSV[tlag]
+        
+        if (Q > 0) {
+          for(n in 1 : Q){
+            vtlag = (t - n*12) - l
+            mmMensalAnual = mmMensalAnual + tht[l, t]*THT[n, t]*residuoSV[vtlag]
+          }
+        }
+      }
+    }
+    
+    if (Q > 0) {
+      for(m in 1 : Q){
+        vlag = v - m*12
+        mmAnual = mmAnual + THT[m, mes] * residuoSV[vlag]
+      }
+    }
+    serieSV[t] = autoMensal + autoAnual - autoMensalAnual - mmMensal - mmAnual + mmMensalAnual + residuoSV[t]
   }
+  serieS = matrix(serieSV, ncol = 12, byrow = T)
   
   dpH = apply(serie, 2, sd)
   dpS = apply(serieS, 2, sd)
