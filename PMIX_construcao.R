b6747c70114c01c1ba1a3e75a88b65f9a54b2003
Leandro Furlam <44095440+leandrofturi@users.noreply.github.com>
2018-12-06 11:47:01 -0200

    Melhoras na saida

--- PMIX_construcao.R
+++ PMIX_construcao.R
@@ -1,7 +1,7 @@
 entrada = function(dados) {
   
   #TRATAMENTO DOS DADOS DE ENTRADA
-
+  
   leitura = read.table(dados, header = T, sep = ";", dec = ",")
   leitura = leitura[, -1]
   serieH = matrix(leitura, ncol = 12, byrow = T)
@@ -13,7 +13,6 @@ entrada = function(dados) {
   
   serieHN = t((t(serieHN) - mPerHN) / dpPerHN)
   
-  #facHN = correlograma(serieHN, nH/4, F)
   final = list(serieH = serieH, serieHN = serieHN, mediaHN = mPerHN, dpHN = dpPerHN)
   return (final)
 }
@@ -21,7 +20,7 @@ entrada = function(dados) {
 correlograma = function(serie, lagMax, grafico) {
   
   #CORRELOGRAMA DE UMA SERIE
-
+  
   mPer = apply(serie, 2, mean)
   dpPer = apply(serie, 2, sd)
   serie = t((t(serie) - mPer))
@@ -64,16 +63,16 @@ correlograma = function(serie, lagMax, grafico) {
 }
 
 residuos = function(serie, parametros, lags) {
-
+  
   #CALCULO DOS RESIDUOS E DO DESVIO-PADRAO
-
+  
   nTotal = length(serie)
   p = lags[1]
   q = lags[2]
   P = lags[3]
   Q = lags[4]
   lagMax = 12*(max(P, Q) + 1)
-
+  
   serieV = numeric(nTotal)
   serieV = as.vector(t(serie))
   
@@ -164,11 +163,11 @@ residuos = function(serie, parametros, lags) {
 }
 
 powell = function (serie, lags, Pinicial) {
-
+  
   #ALGORITMO DE POWELL
   
   eps = 10^-5
-
+  
   p = lags[1]
   q = lags[2]
   P = lags[3]
@@ -228,10 +227,11 @@ powell = function (serie, lags, Pinicial) {
 
 
 secaoAurea = function (serie, lags, ponto, passo) {
-
+  
   #METODO DA SECAO AUREA PARA OBTENCAO DE TRES PONTOS BONS DENTRO DO INTERVALO BUSCADO
-
+  
   OURO = (sqrt(5) - 1) / 2
+  OURO = 5
   OUROlim = 100
   
   a = -1
@@ -322,9 +322,9 @@ secaoAurea = function (serie, lags, ponto, passo) {
 }
 
 metBrent = function (serie, lags, ponto, passo, buscaDourada) {
-
+  
   #METODO DE BRENT PARA OBTENCAO DO TAMANHO DO PASSO
-
+  
   OURO = (sqrt(5) - 1) / 2
   MAXIT = 100
   ab = buscaDourada$a
@@ -427,9 +427,9 @@ metBrent = function (serie, lags, ponto, passo, buscaDourada) {
 }
 
 buscaLinear = function (serie, lags, ponto, passo) {
-
+  
   #FUNCAO GERAL PARA O CALCULO DO TAMANHO DO PASSO
-
+  
   buscaDourada = secaoAurea(serie, lags, ponto, passo)
   metBrent = metBrent(serie, lags, ponto, passo, buscaDourada)
   
@@ -441,16 +441,16 @@ buscaLinear = function (serie, lags, ponto, passo) {
 }
 
 serieSint = function(parametros, dpRes, lags, n) {
-
+  
   #GERACAO DA SERIE SINTETICA
-
+  
   nTotal = 12*n
   p = lags[1]
   q = lags[2]
   P = lags[3]
   Q = lags[4]
   lagMax = 12*(max(P, Q) + 1)
-
+  
   limInf = 0
   limSup = 0
   if (p > 0) {
@@ -476,10 +476,6 @@ serieSint = function(parametros, dpRes, lags, n) {
   
   serieS = matrix(numeric(0), ncol = 12, nrow = n)
   serieSV = numeric(nTotal)
-  dpH = numeric(12)
-  dpS = numeric(12)
-  mediaH = numeric(12)
-  mediaS = numeric(12)
   
   residuoS = matrix(rnorm(nTotal), ncol = 12, nrow = n)
   meanAleat = apply(residuoS, 2, mean)
@@ -488,13 +484,6 @@ serieSint = function(parametros, dpRes, lags, n) {
   residuoS = t(t(residuoS) * dpRes)
   residuoSV = numeric(nTotal)
   residuoSV = as.vector(t(residuoS))
-  
-  autoMensal = numeric(1)
-  autoAnual = numeric(1)
-  autoMensalAnual = numeric(1)
-  mmMensal = numeric(1)
-  mmAnual = numeric(1)
-  mmMensalAnual = numeric(1)
   
   for(t in (lagMax : nTotal)) {
     mes = t %% 12
@@ -552,13 +541,13 @@ serieSint = function(parametros, dpRes, lags, n) {
   return(serieS)
 }
 
-PMIX = function (dados, lags, n) {
-
+PMIX = function (dados, lags, n, seriePlot) {
+  
   #FUNCAO GERAL DO MODELO PMIX
-
+  
   numParametros = sum(lags)*12
   Pinicial = rep(0,numParametros)
-
+  
   entrada = entrada(dados)
   serieHN = entrada$serieHN
   serieH = entrada$serieH
@@ -566,18 +555,127 @@ PMIX = function (dados, lags, n) {
   dpH = apply(serieH, 2, sd)
   mediaHN = entrada$mediaHN
   dpHN = entrada$dpHN
+  facH = correlograma(serieHN, 12, F)
+  intervalop = 1.96 / sqrt(length(serieH)/12)
   
   otimizacao = powell(serieHN, lags, Pinicial)
   parametros = otimizacao$parametros
   ciclos = otimizacao$ciclos
-  dpRes = residuos(serieHN, parametros, lags)$dpRes
+  somRes = otimizacao$somRes
+  paramResiduos = residuos(serieHN, parametros, lags)
+  dpRes = paramResiduos$dpRes
   ciclos = otimizacao$ciclos
-
+  
   serieSN = serieSint(parametros, dpRes, lags, n)
   serieSN = t((t(serieSN) * dpHN) + mediaHN)
   serieS = exp(serieSN)
   mediaS = apply(serieS, 2, mean)
   dpS = apply(serieS, 2, sd)
+  facS = correlograma(serieS, 12, F)
+  
+  p = lags[1]
+  q = lags[2]
+  P = lags[3]
+  Q = lags[4]
+  data = format(Sys.time(), "%F %Hh%M")
+  data = paste0("PMIX (", p, ", ", q, ", ", P, ", ", Q, ") ", data)
+  dir.create(file.path("./", data))
+  if (p > 0) {
+    limInf = 1
+    limSup = 12*p
+    phi = matrix(parametros[limInf : limSup], ncol = 12, byrow = T)
+  }
+  else {
+    p = 1
+    limInf = 1
+    limSup = 12*p
+    phi = matrix(rep(0,12), ncol = 12, byrow = T)
+  }
+  
+  if (q > 0) {
+    limInf = limSup + 1
+    limSup = limInf + 12*q - 1
+    tht = matrix(parametros[limInf : limSup], ncol = 12, byrow = T)
+  }
+  else {
+    q = 1
+    limInf = limSup + 1
+    limSup = limInf + 12*q - 1
+    tht = matrix(rep(0,12), ncol = 12, byrow = T)
+  }
+  
+  if (P > 0) {
+    limInf = limSup + 1
+    limSup = limInf + 12*P - 1
+    PHI = matrix(parametros[limInf : limSup], ncol = 12, byrow = T)
+  }
+  else {
+    P = 1
+    limInf = limSup + 1
+    limSup = limInf + 12*P - 1
+    PHI = matrix(rep(0,12), ncol = 12, byrow = T)
+  }
+  
+  if (Q > 0) {
+    limInf = limSup + 1
+    limSup = limInf + 12*Q - 1
+    THT = matrix(parametros[limInf : limSup], ncol = 12, byrow = T)
+  }
+  else {
+    Q = 1
+    limInf = limSup + 1
+    limSup = limInf + 12*Q - 1
+    THT = matrix(rep(0,12), ncol = 12, byrow = T)
+  }
+  
+  
+  #GERACAO DOS ARQUIVOS DE SAIDA
+  
+  meses = c("JANEIRO", "FEVEREIRO", "MARCO", "ABRIL", "MAIO", "JUNHO", "JULHO", "AGOSTO", "SETEMBRO", "OUTUBRO", "NOVEMBRO", "DEZEMBRO")
+  facNome = paste("AUTOCOR_PER", (0:12))
+  phiNome = paste("phi", (1:p))
+  PHINome = paste("PHI", (1:P))
+  thtNome = paste("tht", (1:q))
+  THTNome = paste("THT", (1:Q))
+  
+  tabelaDadosH = data.frame(facH)
+  rownames(tabelaDadosH) = c(facNome)
+  colnames(tabelaDadosH) = meses
+  write.csv2(tabelaDadosH, paste0(data, "/Autocorrelacao Historica.csv"))
+  
+  tabelaDadosS = data.frame(facS)
+  rownames(tabelaDadosS) = c(facNome)
+  colnames(tabelaDadosS) = meses
+  write.csv2(tabelaDadosS, paste0(data, "/Autocorrelacao Sintetica.csv"))
+  
+  tabelaParametros = data.frame(mediaH, dpH, mediaS, dpS, t(phi), t(PHI), t(tht), t(THT))
+  rownames(tabelaParametros) = meses
+  colnames(tabelaParametros) = c("MEDIA HIST", "SD HIST", "MEDIA SINT", "SD SINT", phiNome, PHINome, thtNome, THTNome)
+  write.csv2(tabelaParametros, paste0(data, "/Parametros.csv"))
+  
+  tabelaserieS = data.frame(serieS)
+  rownames(tabelaserieS) = c(1:n)
+  colnames(tabelaserieS) = meses
+  write.csv2(tabelaserieS, paste0(data, "/Serie Sintetica.csv"))
+  
+  if (seriePlot){
+    par(lwd = 0.5, col = 'grey')
+    plot(main = "Serie Sintética", mediaS, col= 'red', xlim = c(1,12), ylim = c(0, max(serieS)),
+         xlab = "meses", ylab = "valores gerados", type = "n")
+    for(ano in 1:n){
+      points(1:12, serieS[ano, ])
+    }
+    par(col = 'black')
+    legend("topright", c("Dados Históricos", "Dados Sintéticos"), bty = "n", col = c('Blue', 'Red'), pch = 19, cex=0.8)
+    par(lwd = 2)
+    lines(1:12, mediaH, col= 'blue')
+    lines(1:12, mediaS, col= 'red')
+    par(lwd = 0.5)
+    lines(1:12, mediaH + dpH, col= 'blue', lty = 2)
+    lines(1:12, mediaH - dpH, col= 'blue', lty = 2)
+    lines(1:12, mediaS + dpS, col= 'red', lty = 2)
+    lines(1:12, mediaS - dpS, col= 'red', lty = 2)
+  }
   
   final = list(serieH = serieH, serieS = serieS, mediaH = mediaH, mediaS = mediaS, dpH = dpH, dpS = dpS, ciclos = ciclos)
   return (final)
