79bbfb59efb8b8d4144863ec1fcfac59bafb4181
Leandro Furlam Turi <leandrofturi@gmail.com>
2019-07-02 14:29:56 -0300

    ajustes de volume

--- interface/modelo/volumeUtil.R
+++ interface/modelo/volumeUtil.R
@@ -1,5 +1,13 @@
-volumeUtil = function (serie, Pregularizacao, M) {
+volumeUtil = function (Pregularizacao, M) {
+  # O ARQUIVO E ESCOLHIDO DENTRO DO PROGRAMA
+  if (sistema == "Linux")
+    dados = tk_choose.files ( )
+  else if (sistema == "Windows")
+    dados = choose.files ( )
+  
+  serie = leituraSerie (dados)
   nS = length (serie)
+  
   if (M)
     periodo = rep (c (31,28,31,30,31,30,31,31,30,31,30,31), (nS/12))
   else
@@ -7,7 +15,13 @@ volumeUtil = function (serie, Pregularizacao, M) {
   
   periodo = periodo*24*60*60
   # tempoAcumulado = sapply (1:nS, function (x) sum (periodo[1:x]))
-  volumeAcumulado = (serie - (mean (serie)*Pregularizacao))*periodo
+  volumePadronizado = (serie - (mean (serie)*Pregularizacao))*periodo
+  
+  volumeAcumulado = numeric (nS+1)
+  volumeAcumulado[1] = 0
+  for (i in 2:(nS+1)) {
+    volumeAcumulado[i] = volumePadronizado[i-1] + volumeAcumulado[i-1]
+  }
   
   pico = NULL
   i = 2
@@ -18,7 +32,7 @@ volumeUtil = function (serie, Pregularizacao, M) {
         if (((volumeAcumulado[j] > volumeAcumulado[j-1]) && (volumeAcumulado[j] > volumeAcumulado[j+1])) && 
             (volumeAcumulado[j] > volumeAcumulado[i])) {
           pico = c (pico, volumeAcumulado[i] - min (volumeAcumulado[i:j]))
-          i = j
+          i = j-1
           j = nS
         }
         j = j+1
@@ -26,7 +40,15 @@ volumeUtil = function (serie, Pregularizacao, M) {
     }
     i = i+1
   }
-  
   picoMax = max (pico)
   return (picoMax)
+}
+
+leituraSerie = function (dados) {
+  leitura = read.table (dados, header = TRUE, sep = ";", dec = ",")
+  leitura = leitura[, -1]
+  serie = as.matrix (leitura)
+  
+  serie = as.vector (t (serie))
+  return (serie)
 }
